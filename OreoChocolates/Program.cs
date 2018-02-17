3fde5fc6c8c9049c4d8a99db637293bde1e2182c
Darko <dmaric444@gmail.com>
2018-02-17 14:43:01 +0100

    make function to read tags.txt

--- OreoChocolates/Program.cs
+++ OreoChocolates/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SGTIN96;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace OreoChocolates
     {
         static void Main(string[] args)
         {
+            SGTIN96Decoder sgtin96Decoder = new SGTIN96Decoder();
+            var codeList = sgtin96Decoder.GetCodeList(System.IO.Path.GetFullPath(@"..\..\") + @"\Documents\tags.txt");
+
         }
     }
 }
---
+++ SGTIN96/SGTIN96/SGTIN96Decoder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGTIN96
+{
+
+    public class SGTIN96Decoder
+    {
+        public string[] GetCodeList(string filePath)
+        {
+            string[] codes = File.ReadAllLines(filePath);
+            return codes;
+        }
+        
+
+
+    }
+}
